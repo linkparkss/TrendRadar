3bf86dae15fbcb983c751adb826a58f3ed7492fd
linkparkss <linkparkss@users.noreply.github.com>
2026-09-03 22:21:41 +0800

    Revert "feat: add PSASP power-flow convergence tools"
    This reverts commit 55a66889de4d84eb6b05dafd31915a6b778ac894.

--- powerflow-convergence/powerflow/__init__.py
+++
@@ -1,2 +0,0 @@
-"""Power-system automation utilities."""
-
--- powerflow-convergence/通用潮流收敛.py
+++
@@ -1,7 +0,0 @@
-"""Convenience launcher for the journaled manual-PSASP convergence search."""
-
-from powerflow.convergence.cli import main
-
-
-if __name__ == "__main__":
-    raise SystemExit(main())
